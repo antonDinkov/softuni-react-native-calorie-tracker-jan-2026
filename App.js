68d1af932b903f2e488a5deb4bf1087c7a9d1639
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-01-14 21:49:57 +0200

    Memoize total calories

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import { Home, Plus } from 'lucide-react-native';
 import { styles } from './styles';
 import MealSection from './components/meal-section/MealSection';
 import AddMeal from './components/add-meal/AddMeal';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export default function App() {
   const [showAddMeal, setShowAddMeal] = useState(false);
   const [meals, setMeals] = useState([]);
 
-  const totalCalories = meals.reduce((acc, meal) => acc + meal.calories, 0);
+  const totalCalories = useMemo(() => meals.reduce((acc, meal) => acc + meal.calories, 0), [meals]);
 
   const addMealPressHandler = () => {
     setShowAddMeal(true);
